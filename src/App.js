af4c3897d843750690a79d94bb526f1409b1345c
zbigniewstefaniuk <zbyszek791@hotmail.com>
2020-11-19 20:03:43 +0100

    added book skeleton

--- src/App.js
+++ src/App.js
@@ -5,10 +5,47 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       hello
+        <Book />
       </header>
     </div>
   );
 }
+
+function Book() {
+
+  return (
+    <div>
+      <article className='book'>
+        <Image />
+        <Title />
+        <Author />
+      </article>
+
+    </div>)
+}
+
+function Image() {
+
+  return (
+    <img src='https://images-na.ssl-images-amazon.com/images/I/41aM4xOZxaL._SX277_BO1,204,203,200_.jpg' />
+  )
+}
+
+
+
+function Title() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+function Author() {
+  return (<div>
+
+  </div>)
+}
+
 
 export default App;
