fbb8ad2cf25debfac3f8f447a68e94ad0d90d0da
zbigniewstefaniuk <zbyszek791@hotmail.com>
2020-11-19 21:08:03 +0100

    added props to components

--- src/App.js
+++ src/App.js
@@ -1,58 +1,56 @@
-
+import React from 'react'
 import './App.css';
+
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/41aM4xOZxaL._SX277_BO1,204,203,200_.jpg',
+  title: '1984',
+  author: 'George Orwell'
+}
+
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/51SYS7OOBkL._SX376_BO1,204,203,200_.jpg',
+  title: 'Automate the Boring Stuff with Python',
+  author: 'Al Sweigart'
+}
+
+const thirdBook = {
+  img: 'https://m.media-amazon.com/images/I/51zWIiR62qL.jpg',
+  title: 'Django 3 By Example: Build powerful and reliable Python web applications from scratch, 3rd Edition',
+  author: 'Antonio Mele'
+}
 
 function App() {
   return (
     <div className="App">
       <header className="book-list">
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
+        <Book
+          img={firstBook.img}
+          title={firstBook.title}
+          author={firstBook.author} />
+        <Book
+          img={secondBook.img}
+          title={secondBook.title}
+          author={secondBook.author} />
+        <Book
+          img={thirdBook.img}
+          title={thirdBook.title}
+          author={thirdBook.author} />
       </header>
     </div>
-  );
+  )
 }
 
-function Book() {
+function Book(props) {
 
   return (
     <div>
       <article className='book'>
-        <Image />
-        <Title />
-        <Author />
+        <img src={props.img} alt='book' />
+        <h1>{props.title}</h1>
+        <h4>{props.author}</h4>
       </article>
 
     </div>)
 }
-
-function Image() {
-
-  return (
-    <img src='https://images-na.ssl-images-amazon.com/images/I/41aM4xOZxaL._SX277_BO1,204,203,200_.jpg' alt='book cover' />
-  )
-}
-
-
-
-function Title() {
-  return (
-    <div>
-      <h1>1984</h1>
-    </div>
-  )
-}
-
-function Author() {
-  return (
-    <div>
-      <h4>George Orwell</h4>
-    </div>)
-}
-
 
 export default App;
