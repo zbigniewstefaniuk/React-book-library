4fe79ad88cf278aebfcbc7fc0ff3d3845586efa4
zbigniewstefaniuk <zbyszek791@hotmail.com>
2020-11-20 01:02:40 +0100

    added new books to file

--- src/books_db/books.js
+++ src/books_db/books.js
@@ -15,4 +15,23 @@ export const books = [{
     img: 'https://m.media-amazon.com/images/I/51zWIiR62qL.jpg',
     title: 'Django 3 By Example: Build powerful and reliable Python web applications from scratch, 3rd Edition',
     author: 'Antonio Mele'
+},
+{
+    id: 4,
+    img: 'https://m.media-amazon.com/images/I/41ot-wJSOqL.jpg',
+    title: 'Build a Website With Django 3: A complete introduction to Django 3',
+    author: 'Nigel George'
+},
+{
+    id: 5,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51ZgqYqwuAL._SX322_BO1,204,203,200_.jpg',
+    title: 'The Right Stuff',
+    author: 'Tom Wolfe'
+
+},
+{
+    id: 6,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/21vWUE2t23L._SX320_BO1,204,203,200_.jpg',
+    title: 'Mein Kampf',
+    author: 'Adolf Hitler'
 }]
